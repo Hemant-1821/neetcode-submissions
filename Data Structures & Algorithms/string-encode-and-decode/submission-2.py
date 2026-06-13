0a7842fe9fc7cfc9508b6041d5092b602ea8aa45
Hemant Singh <45124643+Hemant-1821@users.noreply.github.com>
2026-06-13 22:59:08 +0100

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        if(len(strs) == 0):
+            return '0'
+        enc_arr = []
+        for i in strs:
+            int_str = ""
+            for alpha in i:
+                int_str += str(ord(alpha)).zfill(3)
+            enc_arr.append(int_str)
+        return ";".join(enc_arr)
+
+    def decode(self, s: str) -> List[str]:
+        if(s == '0'):
+            return []
+        split_arr = s.split(";")
+        final_arr = []
+        for i in split_arr:
+            curr_str = ""
+            for j in range(0, len(i), 3):
+                num = int(i[j:j+3])
+                curr_str += chr(num)
+            final_arr.append(curr_str)
+        return final_arr
