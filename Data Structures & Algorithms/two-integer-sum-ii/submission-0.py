486187ad69d93d365f16fce272283bc4f0390cd5
Hemant Singh <45124643+Hemant-1821@users.noreply.github.com>
2026-06-27 13:28:55 +0100

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i, j = 0, len(numbers)-1
+
+        while True:
+            s = numbers[i] + numbers[j]
+            if s == target:
+                break
+            elif s < target:
+                i += 1
+            else:
+                j -= 1
+        
+        return [i+1, j+1]
+        
